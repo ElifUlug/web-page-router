0dec0ff9b4d60ef319e42d799b9868044b01a1f9
Elif Ulug <47383182+ElifUlug@users.noreply.github.com>
2022-05-30 01:41:18 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <header>
+        <h2>Web Design</h2>
+
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+            >
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+            >
+              ABOUT
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/services"
+              className={({ isActive }) => (isActive ? "activeClass" : "")}
+            >
+              SERVICES
+            </NavLink>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
